9ae2b3365112e71788a290044fb045b9bc4a24fb
Andrew Baxter <>
2025-01-02 03:42:33 +0900

    Fixing nix

--- source/module.nix
+++ source/module.nix
@@ -3,113 +3,122 @@
   options = {
     puteron =
       let
-        submoduleEnum = spec: types.addCheck (types.submodule spec) (v: builtins.length (lib.attrsToList v));
-        upstreamArg = mkOption {
+        submoduleEnum = spec: lib.types.addCheck
+          (lib.types.submodule { options = spec; })
+          (v: builtins.length (lib.attrsToList v) == 1);
+        upstreamArg = lib.mkOption {
           default = null;
-          type = types.nullOr types.attrsOf types.enum [ "strong" "weak" ];
+          type = lib.types.nullOr (lib.types.attrsOf (lib.types.enum [ "strong" "weak" ]));
         };
-        defaultOffArg = mkOption {
+        defaultOffArg = lib.mkOption {
           default = null;
-          type = types.nullOr types.bool;
+          type = lib.types.nullOr lib.types.bool;
         };
-        envArg = _: mkOption {
+        envArg = lib.mkOption {
           default = null;
-          type = types.nullOr types.submodule {
-            clear = mkOption {
-              default = null;
-              type = types.nullOr types.attrsOf types.bool;
+          type = lib.types.nullOr (lib.types.submodule {
+            options = {
+              clear = lib.mkOption {
+                default = null;
+                type = lib.types.nullOr (lib.types.attrsOf lib.types.bool);
+              };
+              add = lib.mkOption {
+                default = null;
+                type = lib.types.nullOr (lib.types.attrsOf lib.types.str);
+              };
             };
-            add = mkOption {
-              default = null;
-              type = types.nullOr types.attrsOf types.str;
-            };
-          };
+          });
         };
-        simpleDurationType = types.strMatching "\\d+[hms]";
-        restartDelayArg = mkOption {
+        simpleDurationType = lib.types.strMatching "\\d+[hms]";
+        durationArg = lib.mkOption {
           default = null;
-          type = types.nullOr simpleDurationType;
+          type = lib.types.nullOr simpleDurationType;
           description = "Like 10s or 5m";
         };
-        stopTimeoutArg = mkOption {
-          default = null;
-          type = types.nullOr simpleDurationType;
-          description = "Like 10s or 5m";
-        };
-        commandArg = mkOption {
-          working_directory = mkOption {
-            default = null;
-            type = types.nullOr types.str;
-          };
-          environment = envArg;
-          command = mkOption {
-            type = types.listOf types.str;
+        commandArg = lib.mkOption {
+          type = lib.types.submodule {
+            options = {
+              working_directory = lib.mkOption {
+                default = null;
+                type = lib.types.nullOr lib.types.str;
+              };
+              environment = envArg;
+              command = lib.mkOption {
+                type = lib.types.listOf lib.types.str;
+              };
+            };
           };
         };
       in
       {
-        enable = mkOption {
-          type = types.bool;
+        enable = lib.mkOption {
+          type = lib.types.bool;
           default = false;
           description = "Enable the puteron service for managing puteron services (tasks)";
         };
         environment = envArg;
-        tasks = mkOption {
+        tasks = lib.mkOption {
           description = "See puteron documentation for field details";
-          type = types.attrsOf submoduleEnum {
-            empty = mkOption {
+          type = lib.types.attrsOf (submoduleEnum {
+            empty = lib.mkOption {
               default = null;
-              type = types.nullOr types.submodule {
-                upstream = upstreamArg;
-                default_off = defaultOffArg;
-              };
+              type = lib.types.nullOr (lib.types.submodule {
+                options = {
+                  upstream = upstreamArg;
+                  default_off = defaultOffArg;
+                };
+              });
             };
-            perpetual = mkOption {
+            perpetual = lib.mkOption {
               default = null;
-              type = types.nullOr types.submodule {
-                upstream = upstreamArg;
-                default_off = defaultOffArg;
-                command = commandArg;
-                started_check = mkOption {
-                  default = null;
-                  type = types.nullOr submoduleEnum {
-                    tcp_socket = mkOption {
-                      default = null;
-                      type = types.nullOr types.str;
-                    };
-                    path = mkOption {
-                      default = null;
-                      type = types.nullOr types.str;
-                    };
+              type = lib.types.nullOr (lib.types.submodule {
+                options = {
+                  upstream = upstreamArg;
+                  default_off = defaultOffArg;
+                  command = commandArg;
+                  started_check = lib.mkOption {
+                    default = null;
+                    type = lib.types.nullOr (submoduleEnum {
+                      tcp_socket = lib.mkOption {
+                        default = null;
+                        type = lib.types.nullOr lib.types.str;
+                      };
+                      path = lib.mkOption {
+                        default = null;
+                        type = lib.types.nullOr lib.types.str;
+                      };
+                    });
                   };
+                  restart_delay = durationArg;
+                  stop_timeout = durationArg;
                 };
-                restart_delay = restartDelayArg;
-                stop_timeout = stopTimeoutArg;
-              };
+              });
             };
-            finite = mkOption {
+            finite = lib.mkOption {
               default = null;
-              type = types.nullOr types.submodule {
-                upstream = upstreamArg;
-                default_off = defaultOffArg;
-                command = commandArg;
-                success_codes = mkOption {
-                  default = null;
-                  type = types.nullOr types.listOf types.int;
+              type = lib.types.nullOr (lib.types.submodule {
+                options = {
+                  upstream = upstreamArg;
+                  default_off = defaultOffArg;
+                  command = commandArg;
+                  success_codes = lib.mkOption {
+                    default = null;
+                    type = lib.types.nullOr (lib.types.listOf lib.types.int);
+                  };
+                  started_action = lib.mkOption {
+                    default = null;
+                    type = lib.types.nullOr lib.types.enum [ "turn_off" "delete" ];
+                  };
+                  restart_delay = durationArg;
+                  stop_timeout = durationArg;
                 };
-                started_action = mkOption {
-                  default = null;
-                  type = types.nullOr types.enum [ "turn_off" "delete" ];
-                };
-                restart_delay = restartDelayArg;
-                stop_timeout = stopTimeoutArg;
-              };
+              });
             };
-            external = mkOption {
+            external = lib.mkOption {
               default = null;
-              type = types.nullOr types.str;
+              type = lib.types.nullOr lib.types.str;
             };
-          };
+          });
         };
       };
   };
@@ -118,6 +127,36 @@
       cfg = config.volumesetup;
     in
     {
+      system.build.puteron_script = pkgs.writeShellScript "puteron-run" (
+        let
+          pkg = import ./package.nix { pkgs = pkgs; };
+          taskDirs = derivation {
+            name = "puteron-task-configs";
+            system = builtins.currentSystem;
+            builder = "${pkgs.python3}/bin/python3";
+            args = [
+              ./module_gendir.py
+              (builtins.toJSON config.puteron.tasks)
+            ];
+          };
+        in
+        lib.concatStringsSep " " [
+          "${pkg}/bin/puteron"
+          "demon"
+          "run"
+          (pkgs.writeText "puteron-config" (builtins.toJSON (builtins.listToAttrs (
+            [ ]
+            ++ (lib.lists.optional (config.puteron.environment != null) {
+              name = "environment";
+              value = config.puteron.environment;
+            })
+            ++ [{
+              name = "task_dirs";
+              value = taskDirs;
+            }]
+          ))))
+        ]
+      );
       systemd.services = lib.mkIf cfg.enable {
         volumesetup = {
           wantedBy = [ "multi-user.target" ];
@@ -125,39 +164,7 @@
           startLimitIntervalSec = 0;
           serviceConfig.Restart = "on-failure";
           serviceConfig.RestartSec = 60;
-          script =
-            let
-              pkg = import ./package.nix;
-              taskDirs = derivation {
-                name = "puteron-task-configs";
-                builder = "${pkgs.python3}/bin/python3";
-                args = [
-                  ./module_gendir.py
-                  (builtins.toJSON (builtins.listToAttrs (builtins.concatMap (lib.attrsToList [
-                    config.puteron.empty
-                    config.puteron.perpetual
-                    config.puteron.finite
-                    config.puteron.external
-                  ]))))
-                ];
-              };
-            in
-            lib.concatStringsSep " " [
-              "${pkg}/bin/puteron"
-              "demon"
-              "run"
-              (pkgs.writeText "puteron-config" (builtins.toJSON (builtins.listToAttrs (
-                [ ]
-                ++ (lib.option config.puteron.environment {
-                  name = "environment";
-                  value = config.puteron.environment;
-                })
-                ++ [{
-                  name = "task_dirs";
-                  value = taskDirs;
-                }]
-              ))))
-            ];
+          script = cfg.system.build.puteron_script;
         };
       };
     };
